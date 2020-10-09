5716d834c6188f8c1865ad83cd98da71815e1fc9
olivierklaver <olivierklaver@users.noreply.github.com>
2020-10-09 15:47:29 +0200

    slow down animation

--- scripts.js
+++ scripts.js
@@ -6,6 +6,6 @@ function handleScroll(event) {
 	// event.deltaY;
 	let productElm = document.querySelector('.product-demo');
 	productElm.style.cssText = "background-position-x: calc("+ Math.floor(frame) +" * -605px);";
-	frame += 0.1;
+	frame += 0.05;
 	if(frame > 3) frame = 0;
 }
