d7e42f9b401dd34985242e347dbfa5a5bc361134
olivierklaver <olivierklaver@users.noreply.github.com>
2020-10-09 15:30:04 +0200

    Add frame animation

--- scripts.js
+++ scripts.js
@@ -1,7 +1,11 @@
 window.addEventListener('wheel', handleScroll);
 
+let frame = 0;
+
 function handleScroll(event) {
 	// event.deltaY;
 	let productElm = document.querySelector('.product-demo');
-	productElm.style.cssText = "border: 5px solid green;";
+	productElm.style.cssText = "background-position-x: calc("+ Math.floor(frame) +" * -605px);";
+	frame += 0.1;
+	if(frame > 3) frame = 0;
 }
